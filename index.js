8ec538a39a0ad0a035ff696252ee368b4608592b
stormstina <stina.nerin@gmail.com>
2023-02-18 10:04:22 +0100

    test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("hej")
+
+let changeView = document.querySelector('#changeView');
+console.log(changeView);
+
+changeView.addEventListener("click", () => {
+
+    location.href = "log-in.html"
+
+})
